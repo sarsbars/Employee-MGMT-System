5a8dcb5b4decbd0bfe2518f62cc8acce1e22e8d6
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-07-24 12:31:16 -0500

    Add project files.

---
+++ Employee MGMT System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_MGMT_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
